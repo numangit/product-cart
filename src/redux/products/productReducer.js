25d1332194119d16dcce56c30bf9589fe9bfddda
numangit <abdulnumanjob@gmail.com>
2023-02-26 17:50:08 +0600

    product reducer structure created

---
+++ src/redux/products/productReducer.js
@@ -0,0 +1,26 @@
+import { productState } from "./productState";
+import { ADDPRODUCT, ADDTOCART, INCREMENT, DECREMENT, DELETECART } from "./actionType";
+
+const cartReducer = (state = productState, action) => {
+    switch (action.payload) {
+        case ADDPRODUCT:
+            console.log(ADDPRODUCT, "clicked");
+            return;
+        case ADDTOCART:
+            console.log(ADDTOCART, "clicked");
+            return;
+        case INCREMENT:
+            console.log(INCREMENT, "clicked");
+            return;
+        case DECREMENT:
+            console.log(DECREMENT, "clicked");
+            return;
+        case DELETECART:
+            console.log(DELETECART, "clicked");
+            return;
+        default:
+            return state;
+    }
+}
+
+export default cartReducer;
