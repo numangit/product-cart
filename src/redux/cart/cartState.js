8879cd7d46bd880a220c0655ec9da08ca21aa718
numangit <abdulnumanjob@gmail.com>
2023-02-26 17:27:35 +0600

    initial states created for each slice

---
+++ src/redux/cart/cartState.js
@@ -0,0 +1,3 @@
+export const cartState = [
+    { id: 1, productName: "Spring and summer shoes", category: "Mens shoes", imageUrl: "https://i.dummyjson.com/data/products/59/thumbnail.jpg", price: 400, quantity: 10 }
+]
---
+++ src/redux/products/productState.js
@@ -0,0 +1,3 @@
+export const productState = [
+    { id: 1, productName: "Women Winter Clothes", category: "Tops", imageUrl: "https://i.dummyjson.com/data/products/40/thumbnail.jpg", price: 600, quantity: 20 }
+]
