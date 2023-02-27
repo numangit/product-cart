cea58c52604a272b01817e4418f2f81ca9ed551c
numangit <abdulnumanjob@gmail.com>
2023-02-27 13:52:14 +0600

    add to cart function optimised

--- src/redux/cart/cartReducer.js
+++ src/redux/cart/cartReducer.js
@@ -5,42 +5,142 @@ const cartReducer = (state = cartState, action) => {
 
     switch (action.type) {
         case ADDTOCART:
-            const updatedCart = [...state];
-            let itemExist = false;
+            // const updatedCart = [...state];
+            // let itemExist = false;
+            // state.forEach((product => {
+            //     if (product.id === action.payload.id) {
+            //         itemExist = true;
+            //     }
+            // }));
 
-            updatedCart.forEach((product => {
-                if (product.id === action.payload.id) {
-                    itemExist = true;
+            // if (updatedCart.length > 0 && !itemExist) {
+            //     updatedCart.push({
+            //         ...action.payload,
+            //         quantity: 1,
+            //     });
+            //     return updatedCart;
+
+            // } else if (updatedCart.length > 0) {
+            //     updatedCart.forEach((product) => {
+            //         if (product.id === action.payload.id) {
+            //             product.quantity = product.quantity + 1;
+            //             return updatedCart;
+            //         }
+            //     });
+
+            // } else if (updatedCart.length === 0) {
+            //     updatedCart.push({
+            //         ...action.payload,
+            //         quantity: 1,
+            //     });
+            //     return updatedCart;
+
+            // } else {
+            //     return updatedCart;
+            // }
+            // return updatedCart;
+
+            // switch (state) {
+            //     case state.length > 0 && !itemExist:
+            //         return [
+            //             ...state,
+            //             {
+            //                 ...action.payload,
+            //                 quantity: 1
+            //             }
+            //         ]
+            //     case state.length > 0:
+            //         return state.map((product) => {
+            //             if (product.id === action.payload.id) {
+            //                 return {
+            //                     ...product,
+            //                     quantity: product.quantity + 1
+            //                 };
+            //             }
+            //             return product;
+            //         })
+            //     case state.length === 0:
+            //         return [
+            //             ...state,
+            //             {
+            //                 ...action.payload,
+            //                 quantity: 1
+            //             }
+            //         ]
+            //     default:
+            //         return state;
+            // }
+
+            ////sucesssssssssssssssssssssssssssss
+
+            // if (state.length > 0 && !itemExist) {
+            //     return [
+            //         ...state,
+            //         {
+            //             ...action.payload,
+            //             quantity: 1
+            //         }
+            //     ]
+            // } else if (state.length > 0) {
+            //     return state.map((product) => {
+            //         if (product.id === action.payload.id) {
+            //             return {
+            //                 ...product,
+            //                 quantity: product.quantity + 1
+            //             };
+            //         }
+            //         return product;
+            //     })
+
+            // } else if (state.length === 0) {
+            //     return [
+            //         ...state,
+            //         {
+            //             ...action.payload,
+            //             quantity: 1
+            //         }
+            //     ]
+
+            // } else {
+            //     return state;
+            // }
+
+            if (state.length > 0) {
+
+                const itemExist = state.find(product => product.id === action.payload.id);
+                console.log(itemExist);
+
+                if (itemExist) {
+                    return state.map((product) => {
+                        if (product.id === action.payload.id) {
+                            return {
+                                ...product,
+                                quantity: product.quantity + 1
+                            };
+                        }
+                        return product;
+                    })
+
+                } else {
+                    return [
+                        ...state,
+                        {
+                            ...action.payload,
+                            quantity: 1
+                        }
+                    ]
                 }
-            }));
-
-            if (updatedCart.length > 0 && !itemExist) {
-                updatedCart.push({
-                    ...action.payload,
-                    quantity: 1,
-                });
-                return updatedCart;
-
-            } if (updatedCart.length > 0) {
-                updatedCart.forEach((product) => {
-                    if (product.id === action.payload.id) {
-                        product.quantity = product.quantity + 1;
-                        return updatedCart;
-                    }
-                });
-
-            } else if (updatedCart.length === 0) {
-                updatedCart.push({
-                    ...action.payload,
-                    quantity: 1,
-                });
-                return updatedCart;
 
             } else {
-                return updatedCart;
+                return [
+                    ...state,
+                    {
+                        ...action.payload,
+                        quantity: 1
+                    }
+                ]
             }
 
-            return updatedCart;
 
         case INCREMENT:
             return state.map((product => {
