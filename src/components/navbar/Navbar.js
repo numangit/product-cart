7a7477aaddc55b4c8369d53ce8e836b561e8e4ee
numangit <abdulnumanjob@gmail.com>
2023-02-28 10:52:32 +0600

    conditional page handling added

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,6 +6,18 @@ const Navbar = ({ setSelectedPage }) => {
 
     const cartQuantity = useSelector((state) => state.cart.reduce((total, cartItem) => total + cartItem.quantity, 0));
 
+    //function to handle home page
+    const homePageHandler = (e) => {
+        e.preventDefault();
+        setSelectedPage("home");
+    };
+
+    //function to handle cart page
+    const cartPageHandler = (e) => {
+        e.preventDefault();
+        setSelectedPage("cart");
+    };
+
     return (
         <nav className="bg-[#171C2A] py-4">
             <div className="navBar">
@@ -14,16 +26,16 @@ const Navbar = ({ setSelectedPage }) => {
                 </a>
                 <div className="flex gap-4">
                     <a
-                        onClick={() => setSelectedPage("home")}
-                        // href="#home"
+                        onClick={homePageHandler}
+                        href="#home"
                         className="navHome"
                         id="lws-home"
                     >
                         Home
                     </a>
                     <a
-                        onClick={() => setSelectedPage("cart")}
-                        // href="cart.html"
+                        onClick={cartPageHandler}
+                        href="cart.html"
                         className="navCart"
                         id="lws-cart"
                     >
